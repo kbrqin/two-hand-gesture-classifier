260c03e21847df9e42b2d91b6462f962760b28f9
Kim Guo <kbrqin@gmail.com>
2025-03-22 08:03:32 -0400

    two hands gesture classifier

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ def main():
     mp_hands = mp.solutions.hands
     hands = mp_hands.Hands(
         static_image_mode=use_static_image_mode,
-        max_num_hands=1,
+        max_num_hands=2,
         min_detection_confidence=min_detection_confidence,
         min_tracking_confidence=min_tracking_confidence,
     )
@@ -123,6 +123,8 @@ def main():
 
         #  ####################################################################
         if results.multi_hand_landmarks is not None:
+            two_hands_pre_processed_landmark_list = []
+            two_hands_pre_processed_point_history_list = []
             for hand_landmarks, handedness in zip(results.multi_hand_landmarks,
                                                   results.multi_handedness):
                 # Bounding box calculation
@@ -133,18 +135,16 @@ def main():
                 # Conversion to relative coordinates / normalized coordinates
                 pre_processed_landmark_list = pre_process_landmark(
                     landmark_list)
+                
+                two_hands_pre_processed_landmark_list.extend(pre_processed_landmark_list)
+
                 pre_processed_point_history_list = pre_process_point_history(
                     debug_image, point_history)
-                # Write to the dataset file
-                logging_csv(number, mode, pre_processed_landmark_list,
-                            pre_processed_point_history_list)
+                
+                two_hands_pre_processed_point_history_list.extend(pre_processed_point_history_list)
 
-                # Hand sign classification
-                hand_sign_id = keypoint_classifier(pre_processed_landmark_list)
-                if hand_sign_id == 2:  # Point gesture
-                    point_history.append(landmark_list[8])
-                else:
-                    point_history.append([0, 0])
+                # Draw landmarks
+                debug_image = draw_landmarks(debug_image, landmark_list)
 
                 # Finger gesture classification
                 finger_gesture_id = 0
@@ -158,16 +158,15 @@ def main():
                 most_common_fg_id = Counter(
                     finger_gesture_history).most_common()
 
-                # Drawing part
-                debug_image = draw_bounding_rect(use_brect, debug_image, brect)
-                debug_image = draw_landmarks(debug_image, landmark_list)
+            # Write to the dataset file
+            logging_csv(number, mode, two_hands_pre_processed_landmark_list,
+                        two_hands_pre_processed_point_history_list)
+            if len(two_hands_pre_processed_landmark_list) == 84: 
+                hands_id = keypoint_classifier(two_hands_pre_processed_landmark_list)
                 debug_image = draw_info_text(
                     debug_image,
-                    brect,
-                    handedness,
-                    keypoint_classifier_labels[hand_sign_id],
-                    point_history_classifier_labels[most_common_fg_id[0][0]],
-                )
+                    keypoint_classifier_labels[hands_id],
+                ) 
         else:
             point_history.append([0, 0])
 
@@ -490,25 +489,11 @@ def draw_bounding_rect(use_brect, image, brect):
 
     return image
 
-
-def draw_info_text(image, brect, handedness, hand_sign_text,
-                   finger_gesture_text):
-    cv.rectangle(image, (brect[0], brect[1]), (brect[2], brect[1] - 22),
-                 (0, 0, 0), -1)
-
-    info_text = handedness.classification[0].label[0:]
-    if hand_sign_text != "":
-        info_text = info_text + ':' + hand_sign_text
-    cv.putText(image, info_text, (brect[0] + 5, brect[1] - 4),
-               cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv.LINE_AA)
-
-    if finger_gesture_text != "":
-        cv.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 60),
-                   cv.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 0), 4, cv.LINE_AA)
-        cv.putText(image, "Finger Gesture:" + finger_gesture_text, (10, 60),
-                   cv.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2,
-                   cv.LINE_AA)
-
+def draw_info_text(image, hand_sign_text):
+    cv.putText(image, hand_sign_text, (10, 90), cv.FONT_HERSHEY_SIMPLEX,
+               1.0, (255, 255, 255), 2, cv.LINE_AA)
+    cv.putText(image, hand_sign_text, (10, 90), cv.FONT_HERSHEY_SIMPLEX,
+               1.0, (255, 255, 255), 2, cv.LINE_AA)
     return image
 
 
